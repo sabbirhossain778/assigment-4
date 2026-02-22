1e199c8b23ef839a715e0dfc62884ab686b88f38
sabbirhossain778 <mshb5778@gmail.com>
2026-02-23 00:17:01 +0600

    reduce code lines

--- js/style.js
+++ js/style.js
@@ -10,14 +10,11 @@ function showOnly(id, clickedButton){
   const selected = document.getElementById(id);
   selected.classList.remove('hidden');
 
-// ৩. বাটন হাইলাইট করার আসল লজিক (এখানেই ম্যাজিক)
-  // প্রথমে সব বাটন থেকে নীল কালার (btn-primary) সরিয়ে নিন
   const buttons = document.querySelectorAll('#btn-options .tab'); 
-  buttons.forEach(btn => {
-    btn.classList.remove('btn-primary', 'tab-active');
-  });
+  for (const btn of buttons) {
+        btn.classList.remove('btn-primary', 'tab-active');
+    }
 
-  // এখন শুধুমাত্র যে বাটনটি ক্লিক করেছেন (clickedButton) সেটিকে নীল করুন
   clickedButton.classList.add('btn-primary', 'tab-active');
 }
 //selected button function
